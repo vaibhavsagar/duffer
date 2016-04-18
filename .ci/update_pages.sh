ebc146197aefd4ca22455cc4faef2b4052f277bb
Vaibhav Sagar <vaibhavsagar@gmail.com>
2016-04-18 22:51:16 +1000

    Build presentation in script.

--- .ci/update_pages.sh
+++ .ci/update_pages.sh
@@ -7,15 +7,18 @@ if [ "$TRAVIS" == "true" ]; then
     git config --global user.email "travis@travis-ci.org"
     git config --global user.name "Travis"
 fi
-# using token clone gh-pages branch
+# Using token, clone gh-pages branch
 git clone --quiet --branch=$BRANCH https://${GH_TOKEN}@github.com/$TARGET_REPO build > /dev/null
-# go into directory and copy data we're interested in to that directory
+# Generate presentation
+pandoc --standalone -t revealjs presentation/presentation.md -o index.html
+# Go into directory and copy data we're interested in to that directory
 cd build
 cp ../index.html .
+# Download a fresh copy of reveal.js
 rm -rf reveal.js
 curl -L https://github.com/hakimel/reveal.js/archive/3.2.0.tar.gz | tar xz
 mv reveal.js* reveal.js
-# add, commit and push files
+# Add, commit and push files
 git add -f .
 git commit -m "Travis build $TRAVIS_BUILD_NUMBER pushed to Github Pages"
 git push -fq origin $BRANCH > /dev/null
