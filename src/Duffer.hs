609b1d226b91ccc7645ea7128d3306a9866f0980
Vaibhav Sagar <vaibhavsagar@gmail.com>
2016-05-14 00:09:28 +0700

    Inline sortableName.

--- src/Duffer.hs
+++ src/Duffer.hs
@@ -88,10 +88,8 @@ sha1Path _ _ = error "Invalid ref provided"
 
 sortEntries :: [TreeEntry] -> [TreeEntry]
 sortEntries = sortOn sortableName . nub
-
-sortableName :: TreeEntry -> String
-sortableName (TreeEntry mode name _) =
-    if mode == 16384 || mode == 57344 then name ++ "/" else name
+    where sortableName (TreeEntry mode name _) =
+            name ++ (if mode == 16384 || mode == 57344 then "/" else "")
 
 -- Generate a stored representation of a git object.
 showObject :: GitObject -> ByteString
