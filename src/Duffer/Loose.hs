56e11b2a6618e84e8027af50193c8f7e7b2839c6
Vaibhav Sagar <vaibhavsagar@gmail.com>
2016-09-05 17:13:53 -0400

    Rename resolveRef and updateRef.

--- src/Duffer/Loose.hs
+++ src/Duffer/Loose.hs
@@ -59,12 +59,12 @@ hasObject ref = do
     path <- asks (sha1Path ref)
     liftIO $ doesFileExist path
 
-resolveRef :: FilePath -> WithRepo (Maybe GitObject)
-resolveRef refPath = do
+resolveGitRef :: FilePath -> WithRepo (Maybe GitObject)
+resolveGitRef refPath = do
     path <- asks (</> refPath)
     ref  <- liftIO $ init <$> readFile path
     readObject ref
 
-updateRef :: FilePath -> GitObject -> WithRepo Ref
-updateRef refPath object = asks (</> refPath) >>= liftIO . (>> return sha1) .
+updateGitRef :: FilePath -> GitObject -> WithRepo Ref
+updateGitRef refPath object = asks (</> refPath) >>= liftIO . (>> return sha1) .
     flip writeFile (append sha1 "\n") where sha1 = hash object
