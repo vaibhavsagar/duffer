b01cacd2292a6f05b356c04d19ec3c9af838b9c8
Vaibhav Sagar <vaibhavsagar@gmail.com>
2016-10-26 15:21:50 -0400

    Add withRepo.

--- src/Duffer/WithRepo.hs
+++ src/Duffer/WithRepo.hs
@@ -5,9 +5,12 @@ module Duffer.WithRepo
     , liftIO
     ) where
 
-import Control.Monad.Trans.Reader (ask, asks, ReaderT)
+import Control.Monad.Trans.Reader (ask, asks, runReaderT, ReaderT)
 import Control.Monad.IO.Class     (liftIO)
 
 import Duffer.Loose.Objects       (Repo)
 
 type WithRepo = ReaderT Repo IO
+
+withRepo :: Repo -> WithRepo a -> IO a
+withRepo = flip runReaderT
