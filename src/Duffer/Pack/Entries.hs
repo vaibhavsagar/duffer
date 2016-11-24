9c5d2da156083b80bab320f3649f507fa019ef5f
Vaibhav Sagar <vaibhavsagar@gmail.com>
2016-11-24 00:13:24 -0800

    Reorganise declarations.

--- src/Duffer/Pack/Entries.hs
+++ src/Duffer/Pack/Entries.hs
@@ -194,12 +194,12 @@ instance Byteable DeltaInstruction where
     toBytes (CopyInstruction offset size) = let
         offsetBytes = toByteList offset
         lenBytes    = toByteList size
-        encodedOff  = encode offsetBytes
-        encodedLen  = encode lenBytes
         offsetBits  = map (>0) offsetBytes
         lenBits     = map (>0) lenBytes
         bools       = True:padFalse lenBits 3 ++ padFalse offsetBits 4
         firstByte   = fromIntegral $ boolsToByte 0 bools
+        encodedOff  = encode offsetBytes
+        encodedLen  = encode lenBytes
         in B.concat [B.singleton firstByte, encodedOff, encodedLen]
         where encode = B.pack . map fromIntegral . reverse . filter (>0)
               padFalse :: [Bool] -> Int -> [Bool]
