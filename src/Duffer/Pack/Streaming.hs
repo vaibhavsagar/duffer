a813e28505777eafa16a88cc8df4067af10cedaa
Vaibhav Sagar <vaibhavsagar@gmail.com>
2016-12-30 00:02:24 +0700

    Heavily refactored packfile streaming.

--- src/Duffer/Pack/Streaming.hs
+++ src/Duffer/Pack/Streaming.hs
@@ -2,73 +2,68 @@
 
 module Duffer.Pack.Streaming (indexPackFile) where
 
-import Control.Arrow (first)
-import Control.Monad.Trans.State.Strict
-import Data.ByteString.Base16 (decode)
-import Data.Maybe (fromJust)
-import qualified Data.ByteString  as B
-import qualified Data.Map.Strict  as M
-import qualified Pipes.Attoparsec as PA
-import qualified Pipes.ByteString as PB
-import qualified Pipes.Zlib       as PZ
-import qualified System.IO        as SI
-import Pipes
-import Prelude hiding (take)
+import Data.ByteString                  (ByteString, append, concat, length)
+import Codec.Compression.Zlib           (CompressionLevel)
+import Control.Arrow                    (first)
+import Control.Monad.Trans.State.Strict (StateT, evalStateT, runStateT, gets)
+import Data.ByteString.Base16           (decode)
+import Data.Map.Strict                  (Map, empty, insert)
+import Data.Maybe                       (fromJust)
+import Pipes                            (Producer, next)
+import Pipes.Attoparsec                 (parse, parseL)
+import Pipes.ByteString                 (fromHandle, drawByte, peekByte)
+import Pipes.Zlib                       (decompress', defaultWindowBits)
+import Prelude hiding                   (length, concat)
+import System.IO                        (openFile, IOMode(ReadMode))
 
 import Duffer.Loose.Parser (parseBinRef)
-import Duffer.Pack.Parser
-import Duffer.Pack.Entries
+import Duffer.Pack.Parser  (parseOffset, parsePackFileHeader, parseTypeLen)
+import Duffer.Pack.Entries (PackObjectType(..), compressToLevel, encodeOffset
+                           ,getCompressionLevel, encodeTypeLen)
 
-type SeparatedEntries = M.Map Int B.ByteString
+type Prod a   = Producer ByteString IO a
+type IntBSMap = Map Int ByteString
 
-emptySeparated :: SeparatedEntries
-emptySeparated = M.empty
-
-indexPackFile :: FilePath -> IO SeparatedEntries
+indexPackFile :: FilePath -> IO IntBSMap
 indexPackFile path = do
-    handle                    <- PB.fromHandle <$> SI.openFile path SI.ReadMode
-    ((start, count), entries) <- runStateT parsePackFileStart handle
-    fst <$> loopEntries entries start count emptySeparated
-    where parsePackFileStart = do
-            (Right (len, count)) <- fromJust <$> PA.parseL parsePackFileHeader
-            return (len, count)
+    ((start, count), entries) <- runStateT parsePackFileStart =<<
+        fromHandle <$> openFile path ReadMode
+    fst <$> loop entries start count empty
+    where parsePackFileStart =
+            fromRight . fromJust <$> parseL parsePackFileHeader
 
-loopEntries
-    :: Producer B.ByteString IO a -- remaining packfile input
-    -> Int                        -- number of bytes read so far
-    -> Int                        -- number of entries remaining
-    -> SeparatedEntries           -- map of offsets to bytestrings
-    -> IO (SeparatedEntries, Producer B.ByteString IO a)
-loopEntries producer _      0         indexedMap = return (indexedMap, producer)
-loopEntries producer offset remaining indexedMap = do
-    (header, ref, decompressedP, level) <- evalStateT getNextEntry producer
-    step   <- next decompressedP
-    let (decompressed, eitherP) = either ((,) "" . return) id step
-    (output, producer') <- advanceToCompletion decompressed eitherP
-    let content     = B.concat [header, ref, compressToLevel level output]
-    let indexedMap' = M.insert offset content indexedMap
-    let offset'     = offset + B.length content
-    let remaining'  = remaining - 1
-    loopEntries producer' offset' remaining' indexedMap'
-    where
-        getNextEntry = do
-            (Right tLen) <- fromJust <$> PA.parse parseTypeLen
-            baseRef <- case fst tLen of
-                OfsDeltaObject -> do
-                    (Right offset) <- fromJust <$> PA.parse parseOffset
-                    return $ encodeOffset offset
-                RefDeltaObject -> do
-                    (Right ref)    <- fromJust <$> PA.parse parseBinRef
-                    return $ fst $ decode ref
-                _              -> return ""
-            remainder <- get
-            let decompressed = PZ.decompress' PZ.defaultWindowBits remainder
-            PB.drawByte
-            levelByte <- fromJust <$> PB.peekByte
-            let level = getCompressionLevel levelByte
-            return (uncurry encodeTypeLen tLen, baseRef, decompressed, level)
-        advanceToCompletion decompressed producer = next producer >>= \case
-            (Left (Left p)) -> return (decompressed, p)
-            (Right (d, p')) ->
-                first (B.append decompressed) <$> advanceToCompletion d p'
-            _               -> error "No idea how to handle Left (Right _)"
+loop :: Prod a -> Int -> Int -> IntBSMap -> IO (IntBSMap, Prod a)
+loop producer _      0         indexedMap = return (indexedMap, producer)
+loop producer offset remaining indexedMap = do
+    (headerRef, decompressedP, level) <- evalStateT getNextEntry producer
+    (output, producer')               <- uncurry advanceToCompletion =<<
+        either ((,) "" . return) id <$> next decompressedP
+    let content     = headerRef `append` compressToLevel level (concat output)
+        indexedMap' = insert offset content indexedMap
+        offset'     = offset + length content
+        remaining'  = remaining - 1
+    loop producer' offset' remaining' indexedMap'
+
+getNextEntry :: StateT (Prod a) IO
+    (ByteString, Prod (Either (Prod a) a), CompressionLevel)
+getNextEntry = do
+    tLen    <- parse' id parseTypeLen
+    baseRef <- case fst tLen of
+        OfsDeltaObject -> parse'  encodeOffset  parseOffset
+        RefDeltaObject -> parse' (fst . decode) parseBinRef
+        _              -> return ""
+    decompressed <- gets (decompress' defaultWindowBits)
+    _            <- drawByte -- The compression level is in the second byte.
+    level        <- getCompressionLevel . fromJust <$> peekByte
+    let headerRef = append (uncurry encodeTypeLen tLen) baseRef
+    return (headerRef, decompressed, level)
+    where parse' convert = fmap (convert . fromRight . fromJust) . parse
+
+advanceToCompletion :: ByteString -> Prod (Either a b) -> IO ([ByteString], a)
+advanceToCompletion decompressed producer = next producer >>= \case
+    Right (d, p')  -> first ((:) decompressed) <$> advanceToCompletion d p'
+    Left (Left p)  -> return ([decompressed], p)
+    Left (Right _) -> error "No idea how to handle Left (Right _)"
+
+fromRight :: Either a b -> b
+fromRight = either (const $ error "Found Left, Right expected") id
