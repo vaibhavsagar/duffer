a09de830673c6ad623dbc4cbaa84e86b4c3a1f6f
Vaibhav Sagar <vaibhavsagar@gmail.com>
2017-01-26 15:02:47 +0700

    Define general conversion function.

--- src/Duffer/WorkObject.hs
+++ src/Duffer/WorkObject.hs
@@ -7,46 +7,61 @@ import qualified Data.Map.Strict as Map
 import qualified Data.ByteString as B
 import qualified Data.Set        as S (Set, toList, insert, empty)
 
-import Data.Functor.Compose (Compose(..))
+import Control.Monad.Trans.Maybe (MaybeT(..))
+import Data.Functor.Compose      (Compose(..))
+import Data.Functor.Identity     (Identity(..))
 import Duffer.Loose.Objects
-import Duffer               (readObject)
+import Duffer                    (readObject)
 import Duffer.WithRepo
 
 type WorkObject = GitObjectGeneric Ref (Map.Map B.ByteString WorkTreeEntry)
 
 data WorkTreeEntry = WorkTreeEntry WorkObject EntryPermission
 
+convert
+    :: Applicative f
+    => (a -> f c)
+    -> (b -> f d)
+    -> GitObjectGeneric a b
+    -> f (GitObjectGeneric c d)
+convert f g = \case
+    Commit{..} -> Commit
+        <$> f commitTreeRef
+        <*> traverse f commitParentRefs
+        <*> pure commitAuthor
+        <*> pure commitCommitter
+        <*> pure commitMessage
+    Tree{..} -> Tree <$> g treeEntries
+    Blob{..} -> pure Blob{..}
+    Tag{..} -> Tag
+        <$> f tagObjectRef
+        <*> pure tagObjectType
+        <*> pure tagName
+        <*> pure tagTagger
+        <*> pure tagAnnotation
+
 workObject :: Ref -> WithRepo (Maybe WorkObject)
-workObject ref = readObject ref >>= \case
-        Nothing  -> return Nothing
-        Just obj -> case obj of
-            Tree entries -> workTreeEntries entries
-            Commit{..}   -> return . Just $ Commit{..}
-            Blob{..}     -> return . Just $ Blob{..}
-            Tag{..}      -> return . Just $ Tag{..}
+workObject ref = runMaybeT $ MaybeT (readObject ref) >>=
+    convert pure (MaybeT . workTreeEntries)
 
-
-workTreeEntries :: S.Set TreeEntry -> WithRepo (Maybe WorkObject)
+workTreeEntries
+    :: S.Set TreeEntry
+    -> WithRepo (Maybe (Map.Map B.ByteString WorkTreeEntry))
 workTreeEntries entries = do
     let entriesL    = S.toList entries
     let filenames   = map entryName  entriesL
     let permissions = map entryPerms entriesL
     children <- getCompose $ traverse (Compose . workObject . entryRef) entriesL
     let wtEntries   = zipWith WorkTreeEntry <$> children <*> pure permissions
-    return $ Tree . Map.fromList . zip filenames <$> wtEntries
+    return $ Map.fromList . zip filenames <$> wtEntries
 
 hashWorkObject :: WorkObject -> Ref
 hashWorkObject = hash . toGitObject
 
 toGitObject :: WorkObject -> GitObject
-toGitObject = \case
-    Commit{..} -> Commit{..}
-    Tag{..}    -> Tag{..}
-    Blob{..}   -> Blob{..}
-    Tree{..}   -> let
-        entries = Map.foldrWithKey
-            (\k e -> S.insert $ makeTreeEntry k e) S.empty treeEntries
-        in Tree entries
+toGitObject = runIdentity . convert pure (Identity . hashEntries)
+    where hashEntries = Map.foldrWithKey
+            (\k e -> S.insert $ makeTreeEntry k e) S.empty
 
 makeTreeEntry :: B.ByteString -> WorkTreeEntry -> TreeEntry
 makeTreeEntry entryName (WorkTreeEntry wo entryPerms) = let
