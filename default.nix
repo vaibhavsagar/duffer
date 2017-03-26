7283be11ca2d2710fb124ea1792101a0f2852a80
Vaibhav Sagar <vaibhavsagar@gmail.com>
2017-03-26 16:24:26 +0700

    Update Nix derivation

--- default.nix
+++ default.nix
@@ -1,26 +1,20 @@
 { mkDerivation, aeson, attoparsec, base, base16-bytestring
-, base64-bytestring, byteable, bytestring, containers, cryptonite
-, digest, directory, filepath, hspec, hspec-expectations, memory
-, mmap, pipes, pipes-attoparsec, pipes-bytestring, pipes-zlib
-, process, QuickCheck, stdenv, text, transformers
+, base64-bytestring, byteable, bytestring, bytestring-tree-builder
+, containers, cryptonite, digest, directory, filepath, hspec
+, memory, mmap, pipes, pipes-attoparsec, pipes-bytestring
+, pipes-zlib, process, QuickCheck, stdenv, text, transformers
 , unordered-containers, utf8-string, zlib
 }:
 mkDerivation {
   pname = "duffer";
   version = "0.1.0.0";
   src = ./.;
-  isLibrary = true;
-  isExecutable = true;
   libraryHaskellDepends = [
     aeson attoparsec base base16-bytestring base64-bytestring byteable
-    bytestring containers cryptonite digest directory filepath memory
-    mmap pipes pipes-attoparsec pipes-bytestring pipes-zlib text
-    transformers unordered-containers utf8-string zlib
-  ];
-  executableHaskellDepends = [
-    aeson attoparsec base byteable bytestring containers digest
-    directory filepath hspec hspec-expectations process QuickCheck
-    transformers utf8-string
+    bytestring bytestring-tree-builder containers cryptonite digest
+    directory filepath memory mmap pipes pipes-attoparsec
+    pipes-bytestring pipes-zlib text transformers unordered-containers
+    utf8-string zlib
   ];
   testHaskellDepends = [
     aeson attoparsec base byteable bytestring containers digest
