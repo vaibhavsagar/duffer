e5c4996a304c354156ddcd431eb5a87564625feb
Vaibhav Sagar <vaibhavsagar@gmail.com>
2017-03-29 12:46:01 +0700

    Add compose helper

--- src/Duffer/Misc.hs
+++ src/Duffer/Misc.hs
@@ -1,7 +1,12 @@
 module Duffer.Misc where
 
+import Data.Functor.Compose (Compose(..))
+
 ifLeft :: (a -> b) -> Either a b -> b
 ifLeft f = either f id
 
 (.:) :: (c -> d) -> (a -> b -> c) -> a -> b -> d
 (.:) = ((.).(.))
+
+compose :: (Compose f g a -> Compose f g b) -> f (g a) -> f (g b)
+compose fn = getCompose . fn . Compose
