abbd3710bbf3326e9ea913985da88103286ae4d9
Vaibhav Sagar <vaibhavsagar@gmail.com>
2017-05-07 08:24:09 -0400

    Formatting

--- duffer-streaming/test/Spec.hs
+++ duffer-streaming/test/Spec.hs
@@ -1,8 +1,9 @@
-import Control.Monad         (join)
-import Data.Foldable         (traverse_)
-import Data.IntMap           (toList)
-import Test.Hspec            (hspec, describe, it, runIO, shouldBe, SpecWith)
-import Pipes.Prelude         (toListM)
+import Control.Monad (join)
+import Data.Foldable (traverse_)
+import Data.IntMap   (toList)
+import Pipes.Prelude (toListM)
+import Test.Hspec    (hspec, describe, it, runIO, shouldBe, SpecWith)
+
 import Duffer.Pack           (getPackIndices, indexedEntryMap, packFile)
 import Duffer.Pack.Streaming (separatePackFile)
 
