a1ddd49266d377e9d9759f2b69a0796c28cb8f42
Vaibhav Sagar <vaibhavsagar@gmail.com>
2017-05-23 07:10:36 +0800

    default.nix was a mistake

--- default.nix
+++
@@ -1,5 +0,0 @@
-haskellPackages: rec {
-  duffer           = haskellPackages.callPackage ./duffer {};
-  duffer-json      = haskellPackages.callPackage ./duffer-json      { inherit duffer; };
-  duffer-streaming = haskellPackages.callPackage ./duffer-streaming { inherit duffer; };
-}
