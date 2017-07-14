6d5e44ca52a4cb0d1324fab2927ce255e0861cd9
Vaibhav Sagar <vaibhavsagar@gmail.com>
2017-07-14 08:47:23 +0800

    Use callCabal2nix where possible

--- release.nix
+++ release.nix
@@ -1,8 +1,10 @@
-let pkgs = import <nixpkgs> { };
+let
+  pkgs = import <nixpkgs> { };
+  pipes-zlib = pkgs.haskell.lib.doJailbreak pkgs.haskellPackages.pipes-zlib;
 
 in rec {
   duffer           = pkgs.haskellPackages.callPackage ./duffer           {};
-  duffer-streaming = pkgs.haskellPackages.callPackage ./duffer-streaming { inherit duffer; };
-  duffer-json      = pkgs.haskellPackages.callPackage ./duffer-json      { inherit duffer; };
-  ihaskell-duffer  = pkgs.haskellPackages.callPackage ./ihaskell-duffer  { inherit duffer; };
+  duffer-streaming = pkgs.haskellPackages.callCabal2nix "duffer-streaming" ./duffer-streaming { inherit duffer; pipes-zlib = pipes-zlib; };
+  duffer-json      = pkgs.haskellPackages.callCabal2nix "duffer-json"      ./duffer-json      { inherit duffer; };
+  ihaskell-duffer  = pkgs.haskellPackages.callCabal2nix "ihaskell-duffer"  ./ihaskell-duffer  { inherit duffer; };
 }
