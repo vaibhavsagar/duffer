8532a5d44d1af1f832a8f21623f182010e959d58
Vaibhav Sagar <vaibhavsagar@gmail.com>
2017-07-18 08:51:16 +0800

    Move derivations to release.nix

--- release.nix
+++ release.nix
@@ -2,10 +2,11 @@ let
   pkgs       = import <nixpkgs> { };
   pipes-zlib = pkgs.haskell.lib.doJailbreak pkgs.haskellPackages.pipes-zlib;
   cabal2nix  = pkgs.haskellPackages.callCabal2nix;
+  dontCheck  = pkgs.haskell.lib.dontCheck;
 
 in rec {
-  duffer           = pkgs.haskellPackages.callPackage ./duffer           {};
-  duffer-streaming = cabal2nix "duffer-streaming"     ./duffer-streaming { inherit duffer; pipes-zlib = pipes-zlib; };
-  duffer-json      = cabal2nix "duffer-json"          ./duffer-json      { inherit duffer; };
-  ihaskell-duffer  = cabal2nix "ihaskell-duffer"      ./ihaskell-duffer  { inherit duffer; };
+  duffer           = dontCheck (cabal2nix "duffer"           ./duffer           {});
+  duffer-streaming = dontCheck (cabal2nix "duffer-streaming" ./duffer-streaming { inherit duffer; pipes-zlib = pipes-zlib; });
+  duffer-json      = dontCheck (cabal2nix "duffer-json"      ./duffer-json      { inherit duffer; });
+  ihaskell-duffer  = dontCheck (cabal2nix "ihaskell-duffer"  ./ihaskell-duffer  { inherit duffer; });
 }
