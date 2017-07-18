e0c332ece02a60794628ddba6d04096f6e5903c8
Vaibhav Sagar <vaibhavsagar@gmail.com>
2017-07-18 08:39:12 +0800

    Refactor multiple cabal2nix calls

--- release.nix
+++ release.nix
@@ -1,10 +1,11 @@
 let
-  pkgs = import <nixpkgs> { };
+  pkgs       = import <nixpkgs> { };
   pipes-zlib = pkgs.haskell.lib.doJailbreak pkgs.haskellPackages.pipes-zlib;
+  cabal2nix  = pkgs.haskellPackages.callCabal2nix;
 
 in rec {
   duffer           = pkgs.haskellPackages.callPackage ./duffer           {};
-  duffer-streaming = pkgs.haskellPackages.callCabal2nix "duffer-streaming" ./duffer-streaming { inherit duffer; pipes-zlib = pipes-zlib; };
-  duffer-json      = pkgs.haskellPackages.callCabal2nix "duffer-json"      ./duffer-json      { inherit duffer; };
-  ihaskell-duffer  = pkgs.haskellPackages.callCabal2nix "ihaskell-duffer"  ./ihaskell-duffer  { inherit duffer; };
+  duffer-streaming = cabal2nix "duffer-streaming"     ./duffer-streaming { inherit duffer; pipes-zlib = pipes-zlib; };
+  duffer-json      = cabal2nix "duffer-json"          ./duffer-json      { inherit duffer; };
+  ihaskell-duffer  = cabal2nix "ihaskell-duffer"      ./ihaskell-duffer  { inherit duffer; };
 }
