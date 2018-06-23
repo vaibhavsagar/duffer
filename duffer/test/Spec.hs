2d4bbe1608247a389f789935df0ec89d2cf85084
Vaibhav Sagar <vaibhavsagar@gmail.com>
2018-06-23 14:22:14 +0800

    duffer/test/Spec.hs: run more tests in parallel

--- duffer/test/Spec.hs
+++ duffer/test/Spec.hs
@@ -46,9 +46,9 @@ main :: IO ()
 main = do
     let objectTypes = ["blob", "tree", "commit", "tag"]
     allTypesObjects <- traverse objectsOfType objectTypes
-    hspec . describe "packed representation" $ do
-        parallel $ testEncodingAndParsing
-        parallel $ testReading "packed" objectTypes allTypesObjects
+    hspec . parallel . describe "packed representation" $ do
+        testEncodingAndParsing
+        testReading "packed" objectTypes allTypesObjects
         testUnpackingAndWriting =<< runIO (getPackIndices $ repo </> "objects")
     hspec . parallel . describe "loose representation" $ do
         testReading "loose" objectTypes allTypesObjects
