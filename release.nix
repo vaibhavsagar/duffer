c9cc8ac47f9e21589a0fdde13e830a9021fbca83
Vaibhav Sagar <vaibhavsagar@gmail.com>
2020-01-08 13:48:17 -0500

    duffer-json: use new base64

--- release.nix
+++ release.nix
@@ -4,6 +4,10 @@ let
     mkdir -p $out
     cp -r $src/.git/* $out/
   '';
+  base64-src = builtins.fetchTarball {
+    url = "https://github.com/emilypi/base64/tarball/fab53ab62e115bf21986e91ee539ea8764db9a7a";
+    sha256 = "1d7pv6fp3m3ny7iayzss6iqyp0jd1sd5w11pviy8sn112vbmjj52";
+  };
   testPatch  = prj: pkgs.haskell.lib.overrideCabal prj (oldDrv: {
     testSystemDepends = (oldDrv.testSystemDepends or []) ++ [ pkgs.git ];
     postPatch = if pkgs.lib.inNixShell then "" else ''
@@ -21,6 +25,7 @@ let
       drv = testPatch (self.callCabal2nix (builtins.baseNameOf path) path args);
     in drv;
   in {
+    base64 = self.callCabal2nix "base64" base64-src {};
     duffer = produce ./duffer {};
     duffer-json = produce ./duffer-json {};
     duffer-streaming = produce ./duffer-streaming {};
